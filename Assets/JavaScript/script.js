e0a32212133246e83e6c18963df89513558f7628
matronae <mykaomas@gmail.com>
2023-10-07 12:01:06 -0500

    added timer element to quiz when button is pressed the countdown begins

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,13 +1,66 @@
 // Create variable to hide first section
-let hideLandingPage = document.querySelector("#landing-page");
+let landingPage = document.querySelector("#landing-page");
+let questionsPage = document.querySelector("questions");
+let timerEl = document.querySelector('#countdown')
 
-// Hides first section once botton is pressed
-function hideFirstSec() {
-    console.log('button pressed')
-    if (hideLandingPage.style.display === "none") {
-    hideLandingPage.style.display = "block";
-    } else {
-    hideLandingPage.style.display = "none";
+
+let secLeft = 75
+
+function startGame() {
+// Hides landing page once button is pressed
+    console.log('button pressed');
+    landingPage.style.display = "none";
+
+// Starts the countdown of the timer
+    console.log('timer started')
+    let timeLeft = setInterval(function() {
+    secLeft--;
+    timerEl.textContent = secLeft;
+
+    if(secLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timeLeft);
+    //   // Calls function to create and append image
+    //   sendMessage();
+    //
     }
+
+  }, 1000);
+
+
 }
+
+// Put questions into an object
+
+// const firstQuestions = [
+//     {
+//         prompt: 'Commonly Used Data Types DO NOT include: ',
+//         choices: ['string', 'boolean', 'numbers', 'alerts'],
+//         answer: 'alerts'
+//     }
+// ]
+
+// const secondQuestions = [
+//     {
+//         prompt: 'The condition in an if / else statement is enclosed within _____. ',
+//         choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+//         answer: 'parentheses'
+//     }
+// ]
+
+// const thirdQuestions = [
+//     {
+//         prompt: 'Arrays in JavaScript can be used to store ______. ',
+//         choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+//         answer: 'alerts'
+//     }
+// ]
+
+// const fouthQuestions = [
+//     {
+//         prompt: 'String values must be enclosed within _______ when being assigned to variables.',
+//         choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+//         answer: 'quotes'
+//     }
+// ]
 
